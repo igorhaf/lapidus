c3f77a31833dd407344886fe44c29ea484598878
Igor Herson <igorhaf@gmail.com>
2025-07-17 19:26:50 -0300

    integrating with laravel

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\Home\PaginaInicialController;
+use App\Http\Controllers\Api\V1\Home\HomeController;
 
 Route::prefix('api/v1')
     //->middleware(['auth:sanctum', 'throttle:api'])
     ->middleware(['throttle:api'])
     ->group(function() {
-        Route::apiResource('pagina-inicial', PaginaInicialController::class);
+        Route::apiResource('pagina-inicial', HomeController::class);
     });
