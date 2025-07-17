0cf0f09eed7f9a2b1007c106e93f34e3c2cdc776
Igor Herson <igorhaf@gmail.com>
2025-07-17 19:55:59 -0300

    integrating with laravel

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 use App\Http\Controllers\Api\V1\Home\PaginaInicialController;
 
-Route::prefix('api/v1')
+// Definição do rate limiter 'api'
+RateLimiter::for('api', function ($request) {
+    return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+});
+
+Route::prefix('v1')
     ->middleware(['throttle:api'])
     ->group(function() {
         Route::apiResource('pagina-inicial', PaginaInicialController::class);
